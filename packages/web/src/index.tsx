3b4f89d53e94a60064dff8b7ee9c4624b0fd83ea
Bruno Papa <bruno.papa@ifood.com.br>
2023-04-26 00:17:36 -0300

    web: enable vercel web analytics

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-
 import App from './components/app'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 render(<App />, document.getElementById('root'))
